523e610a8aaed0f1907578ad69b00cc64fccbb5f
MarceloHabreu <marcelohenriqueas17@gmail.com>
2025-01-08 19:57:35 -0300

    feat: formart registration date updated

--- sales-backend/src/main/java/io/github/marcelohabreu/sales_api/DTO/ProductRequestDTO.java
+++ sales-backend/src/main/java/io/github/marcelohabreu/sales_api/DTO/ProductRequestDTO.java
@@ -1,14 +1,17 @@
 package io.github.marcelohabreu.sales_api.DTO;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record ProductFormDTO(
-        String sku,
+public record ProductRequestDTO(
+        Long id, String sku,
         String name,
         String description,
         BigDecimal price,
+        @JsonFormat(pattern = "dd/MM/yyyy")
         LocalDate registration) {
 
 }
